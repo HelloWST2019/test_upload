b129987ed7c7cc1ce6aa82ac752a003c741aa43e
sec_dlp <sec_dlp@hypergryph.com>
2026-06-11 16:37:14 +0800

    初次提交测试文件

---
+++ java-testFile.java
@@ -0,0 +1,17 @@
+public class Dlptest {
+    public static void main(String[] args) {
+        System.out.println("姓名	身份证	地址	电话	邮箱");
+        System.out.println("张伟	110101191003075713	北京东城	18617371418	18617371418@dlptest.com");
+        System.out.println("王伟	120101191008033127	天津河西	18665617175	18665617175@dlptest.com");
+        System.out.println("王芳	130102191004028214	河北石家庄	18668185066	18668185066@dlptest.com");
+        System.out.println("李伟	130102191004029815	河北石家庄	18648735683	18648735683@dlptest.com");
+        System.out.println("王秀英	110101191003075334	北京西城	18639096384	18639096384@dlptest.com");
+        System.out.println("李秀英	140105191001018898	山西太原	18676534383	18676534383@dlptest.com");
+        System.out.println("李娜	140105191001018716	山西大同	18601676702	18601676702@dlptest.com");
+        System.out.println("张秀英	120101191002019682	天津南开	18613659933	18613659933@dlptest.com");
+        System.out.println("刘伟	210102191003071835	辽宁沈阳	18627116473	18627116473@dlptest.com");
+        System.out.println("张敏	210102191003073072	辽宁沈阳	18641553402	18641553402@dlptest.com");
+        System.out.println("王英	110101191003075323	北京西城	18639096382	18232332324@dlptest.com");
+
+    }
+}
